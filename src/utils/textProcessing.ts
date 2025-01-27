e069e874f7058d0990b5ccb6afd5f22a3947d2ff
Andrew Apple <andrewdapple@gmail.com>
2025-01-27 11:35:30 -0500

    Update textProcessing.ts
    fixed preview issue

--- src/utils/textProcessing.ts
+++ src/utils/textProcessing.ts
@@ -1,3 +1,44 @@
+export const MAX_TOTAL_CHARS = 12000;
+export const MAX_IMAGES = 20;
+export const MAX_CHARS_PER_SLIDE = 600;
+export function splitTextIntoChunks(text: string): string[] {
+  const chunks: string[] = [];
+  const paragraphs = text.split(/\n\n\n/);
+  let currentChunk = "";
+  
+  for (let i = 0; i < paragraphs.length; i++) {
+    const paragraph = paragraphs[i].trim();
+    const paragraphParts = paragraph.split(/\n\n/);
+    
+    for (let part of paragraphParts) {
+      if (currentChunk && (currentChunk + "\n\n" + part).length > MAX_CHARS_PER_SLIDE) {
+        chunks.push(currentChunk);
+        currentChunk = part;
+      } else {
+        currentChunk = currentChunk ? currentChunk + "\n\n" + part : part;
+      }
+      
+      if (currentChunk.length >= MAX_CHARS_PER_SLIDE) {
+        chunks.push(currentChunk);
+        currentChunk = "";
+      }
+    }
+    
+    if (i < paragraphs.length - 1 || currentChunk.length >= MAX_CHARS_PER_SLIDE) {
+      if (currentChunk) {
+        chunks.push(currentChunk);
+        currentChunk = "";
+      }
+    }
+  }
+  
+  if (currentChunk) {
+    chunks.push(currentChunk);
+  }
+  return chunks.slice(0, MAX_IMAGES);
+}
+
+/*
 export const MAX_CHARS_PER_SLIDE = 600;
 export const MAX_TOTAL_CHARS = 12000;
 
@@ -38,7 +79,7 @@ export const splitTextIntoChunks = (text: string): string[] => {
   }
 
   return chunks;
-};
+};*/
 
 interface ImageBreakInfo {
   position: number;
